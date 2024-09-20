e27e30ea8b9bb0b3b397c6b0aaa0569b8e457372
IrinaV <vasilyeva.irina18@gmail.com>
2024-09-20 10:12:59 +0300

    1st version

---
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactory.swift
+//  MovieQuiz
+//
+//  Created by Irina Vasileva on 19.09.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Irina Vasileva on 20.09.2024.
+//
+
+import Foundation
